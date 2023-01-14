f59929e863b96d1dc90fc1e2568fb6bd0516d683
camkurup <camkurup@hotmail.com>
2023-01-14 12:54:01 +0100

    Mock

---
+++ src/app/Mocks/mock-dash-temperature.ts
@@ -0,0 +1,6 @@
+import { DashTemperature } from "../Interfaces/dash-temp";
+import { Temperature } from "../Interfaces/temperature";
+
+export const TEMPERATURES: DashTemperature[] = [
+    {Temperature: 26, UVLightOn: true}
+];

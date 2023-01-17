ede65f9ee69aa6203faa6e54c7e6a078810a68fd
camkurup <camkurup@hotmail.com>
2023-01-17 10:08:35 +0100

    små tilføjelser

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { LightComponent } from './light/light.component';
 import { TemperatureComponent } from './temperature/temperature.component';
 
 const routes: Routes = [
   {path:"", component: DashboardComponent},
-  {path:"temperature", component: TemperatureComponent}];
+  {path:"temperature", component: TemperatureComponent},
+  {path:"light", component: LightComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

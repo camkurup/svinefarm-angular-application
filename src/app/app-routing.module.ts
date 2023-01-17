f3ffb747aaaa00f83a24f892aa6fd16c5ee99c28
camkurup <camkurup@hotmail.com>
2023-01-17 10:02:11 +0100

    Frobindelse mellem de forskellige compoments er op

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TemperatureComponent } from './temperature/temperature.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component: DashboardComponent},
+  {path:"temperature", component: TemperatureComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
